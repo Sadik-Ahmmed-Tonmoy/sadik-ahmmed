9fcd9edf4eaf05d5a85bbb251cbab0f8657e3d61
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-12-19 01:40:41 +0600

    .env

--- src/redux/ReduxStoreProvider.tsx
+++ src/redux/ReduxStoreProvider.tsx
@@ -1,18 +1,27 @@
 "use client";
+
 import { AppStore, makeStore } from "@/redux/store";
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
-export default function ReduxStoreProvider({ children }: { children: React.ReactNode }) {
+export default function ReduxStoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Initialize useRef with null as the default value
+  const storeRef = useRef<{
+    store: AppStore;
+    persistor: ReturnType<typeof persistStore>;
+  } | null>(null);
 
-  const storeRef = useRef<{ store: AppStore; persistor: ReturnType<typeof persistStore> }>();
-
+  // Create store and persistor only if not already initialized
   if (!storeRef.current) {
-    const store = makeStore();
-    const persistor = persistStore(store);
-    storeRef.current = { store, persistor };
+    const store = makeStore(); // Create Redux store
+    const persistor = persistStore(store); // Create persistor
+    storeRef.current = { store, persistor }; // Set the ref's current value
   }
 
   return (
